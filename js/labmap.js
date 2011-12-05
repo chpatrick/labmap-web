6b4621bf7988b48e969cc9a9400c94e243083374
Patrick Chilton <chpatrick@gmail.com>
2011-12-05 15:30:10 +0000

    Seperated out frontend and common parts.

---
+++ js/labmap.js
@@ -0,0 +1,91 @@
+/* Use labmap namespace */
+labmap = {}
+labmap.svgmap = null;
+labmap.machines = [];
+labmap.working = true;
+
+function User(username, full_name, imageURL){
+    var name = full_name.split(" ");
+    
+    return {
+        username : username,
+        first_name : name[0],
+        last_name : name[1],
+        imageURL : imageURL
+    }
+}
+
+function Machine(machine_name, user){
+    return {
+        machinename : machine_name,
+        user : user,
+        tableRow : null,
+        mapImage : null,
+        mapElement : null
+    }
+}
+
+$(document).ready(function() {
+    $('#lab_map').svg({
+        loadURL: "labmap.svg",
+	changeSize: true,
+        onLoad: function(svg){
+            console.log(svg);
+            labmap.svgmap = svg;
+            reloadLabMapData();
+            setInterval(function() { reloadLabMapData(); }, 10000);
+        }
+    });
+});
+
+function updateMap(){
+    var image = null;
+    var xlink = "http://www.w3.org/1999/xlink";
+
+    $.each(labmap.machines, function(index, m){
+        m.mapElement = $('#' + m.machinename);
+        m.mapImage = $(m.mapElement, labmap.svgmap.root()).get(0);
+
+        if(m.mapImage){
+            if(m.user){
+                m.mapImage.setAttributeNS(xlink, 'href', m.user.imageURL);
+                var title = m.mapElement.attr('title');
+
+                if (!title) {
+                    m.mapElement.attr('title', m.machinename + ': ' + m.user.first_name + ' ' + m.user.last_name + ' (' + m.user.username + ')');
+                }
+            }else{
+                m.mapImage.hide();
+            }
+        }
+    });
+}
+
+function mapUpdated() {}
+
+function reloadLabMapData() {
+    if(!labmap.working) return;
+    
+    var machines = []
+    var u = null;
+    var m = null;
+    $.getJSON('labmap.json', function(data) {
+        if(data.closed){
+            labmap.working = false;
+            jQuery.facebox('Lab\'s closed, get some sleep. :)');
+        }else if(data.stopped){
+            labmap.working = false;
+            jQuery.facebox('Nothing to see here, move along.');
+        }else{
+            $.each(data, function(host, user) {
+                u = User(user.username, user.fullname, user.image);
+                m = Machine(host, u);
+                machines.push(m);
+            });
+
+            labmap.machines = machines; //Replace listing
+            updateMap();
+	    mapUpdated();
+        }
+    });
+}
--- js/main.js
+++ js/main.js
@@ -1,69 +1,6 @@
-/* Use labmap namespace */
-labmap = {}
-labmap.svgmap = null;
-labmap.machines = [];
-labmap.working = true;
-
-function User(username, full_name, imageURL){
-    var name = full_name.split(" ");
-    
-    return {
-        username : username,
-        first_name : name[0],
-        last_name : name[1],
-        imageURL : imageURL
-    }
-}
-function Machine(machine_name, user){
-    return {
-        machinename : machine_name,
-        user : user,
-        tableRow : null,
-        mapImage : null,
-        mapElement : null
-    }
-}
-
-$(document).ready(function() {
-    $('#lab_map').svg({
-        loadURL: "labmap.svg",
-	changeSize: true,
-        onLoad: function(svg){
-            console.log(svg);
-            labmap.svgmap = svg;
-            reloadLabMapData();
-            setInterval(function() { reloadLabMapData(); }, 10000);
-        }
-    });
-});
-
-function reloadLabMapData() {
-    if(!labmap.working) return;
-    
-    var machines = []
-    var u = null;
-    var m = null;
-    $.getJSON('labmap.json', function(data) {
-        if(data.closed){
-            labmap.working = false;
-            jQuery.facebox('Lab\'s closed, get some sleep. :)');
-        }else if(data.stopped){
-            labmap.working = false;
-            jQuery.facebox('Nothing to see here, move along.');
-        }else{
-            $.each(data, function(host, user) {
-                u = User(user.username, user.fullname, user.image);
-                m = Machine(host, u);
-                machines.push(m);
-            });
-
-            console.log(machines);
-            labmap.machines = machines; //Replace listing
-            updateTable();
-            updateMap();
-            setupListenersForMachines();
-        }
-    });
+function mapUpdated() {
+    updateTable();
+    setupListenersForMachines();
 }
 
 function updateTable(){
@@ -91,28 +28,6 @@ function updateTable(){
 //        .tablesorterPager({positionFixed:false, size:18, container: $("#pager")});
 }
 
-function updateMap(){
-    var image = null;
-    var xlink = "http://www.w3.org/1999/xlink";
-
-    $.each(labmap.machines, function(index, m){
-        m.mapElement = $('#' + m.machinename);
-        m.mapImage = $(m.mapElement, labmap.svgmap.root()).get(0);
-
-        if(m.mapImage){
-            if(m.user){
-                m.mapImage.setAttributeNS(xlink, 'href', m.user.imageURL);
-                var title = m.mapElement.attr('title');
-
-                if (!title) {
-                    m.mapElement.attr('title', m.machinename + ': ' + m.user.first_name + ' ' + m.user.last_name + ' (' + m.user.username + ')');
-                }
-            }else{
-                m.mapImage.hide();
-            }
-        }
-    });
-}
 
 function setupListenersForMachines(){
     $.each(labmap.machines, function(index, m){
@@ -134,25 +49,3 @@ function showUserDataForMachine(m){
     $("#user_info_username").html('<a href="mailto:' + m.user.username + '@ic.ac.uk">' + m.user.username + '</a>');
     $("#user_info_machine").html('<a href="ssh://' + m.machinename + '.doc.ic.ac.uk">' + m.machinename + '</a>');
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
